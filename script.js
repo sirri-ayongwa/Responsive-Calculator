e13c1e48adbda17b2c5bb66528545467010624ac
Sirri Ayongwa <105353083+sirri-ayongwa@users.noreply.github.com>
2024-09-10 04:03:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function addToScreen(value) {
+    document.getElementById('screen').value += value;
+  }
+  
+  function clearScreen() {
+    document.getElementById('screen').value = '';
+  }
+  
+  function calculate() {
+    try {
+      var result = eval(document.getElementById('screen').value);
+      document.getElementById('screen').value = result;
+    } catch (error) {
+      document.getElementById('screen').value = 'Error';
+    }
+  }
+  
